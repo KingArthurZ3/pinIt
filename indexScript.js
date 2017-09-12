f4ae1acb7ac5c206443496a16f75bb5d60a02404
Arthur Zhang <arthur@Arthurs-MacBook-Pro.local>
2017-09-12 00:12:13 -0700

    added file upload feature

--- indexScript.js
+++ indexScript.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     $("#newbg").hide();
 
     logBtn.addEventListener('click', function(){
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider);
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider);
     })
 
     var userObj = "";
@@ -19,21 +19,72 @@ $(document).ready(function(){
     function uiRefresh(){
         $("#newbg").hide();
         $('#logOut').hide();
+        $('#intro').show();
+        $('.container').show();
     }
 
     function onAuthStateChanged(user){
         if(user){
             userObj = user
             //set global var userObj to user
-            $("#display-name").text("Logged in: " + user.displayName);
+            $("#display-name").text("Welcome, " + user.displayName + ".");
 
             $("#newbg").show();
 
             $('#logOut').show();
+
+            $('#intro').hide();
+
+            $('.container').hide();
         }
         else{
             uiRefresh();
         }
 
     }
+
+    // info for uploading images
+
+    var uploader = document.getElementById('uploader');
+    var fileButton = document.getElementById('fileButton');
+
+    // Listen for file selection
+    fileButton.addEventListener('change', function(e){
+
+        var file = e.target.files[0];
+
+        //Get File
+            var storageRef = firebase.storage().ref('artsy_photos/' + file.name)
+        //Create a Storage Ref
+            var task = storageRef.put(file);
+        // Upload file
+
+        //Update Progress Bar
+            task.on('state_changed',
+                function progress(snapshot) {
+                    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    uploader.value = percentage;
+                    //TODO change the storage rule to if request.auth != null later
+                },
+
+                function error(err){
+
+                },
+
+                function complete(){
+                    var newFile = firebase.database().ref('artsy_photos/' + file.name)
+                    addImage(newFile);
+                }
+
+            );
+    });
+
+    function addImage(file){
+        var img = document.createElement("img");
+        img.src= file;
+        img.id = "picture"
+        var grid = document.getElementById("photoGrid")
+        grid.appendChild(img);
+    }
+
 })
